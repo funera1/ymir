99b944808316fad3b7e051f2739272d89bdbed76
funera1 <funeral7776@gmail.com>
2025-01-26 19:45:37 +0900

    bootloader template

--- build.zig
+++ build.zig
@@ -15,4 +15,36 @@ pub fn build(b: *std.Build) void {
         .linkage = .static,
     });
     b.installArtifact(surtr);
+
+    // EFI directory
+    const out_dir_name = "img";
+    const install_surtr = b.addInstallFile(
+        surtr.getEmittedBin(),
+        b.fmt("{s}/efi/boot/{s}", .{ out_dir_name, surtr.name }),
+    );
+    install_surtr.step.dependOn(&surtr.step);
+    b.getInstallStep().dependOn(&install_surtr.step);
+
+    const qemu_args = [_][]const u8 {
+        "qemu-system-x86_64",
+        "-m",
+        "512M",
+        "-bios",
+        "/usr/share/ovmf/OVMF.fd",
+        "-drive",
+        b.fmt("file=fat:rw:{s}/{s},format=raw", .{ b.install_path, out_dir_name }),
+        "-nographic",
+        "-serial",
+        "mon:stdio",
+        "-no-reboot",
+        "-enable-kvm",
+        "-cpu",
+        "host",
+        "-s",
+    };
+    const qemu_cmd = b.addSystemCommand(&qemu_args);
+    qemu_cmd.step.dependOn(b.getInstallStep());
+
+    const run_qemu_cmd = b.step("run", "Run QEMU");
+    run_qemu_cmd.dependOn(&qemu_cmd.step);
 }
