0f921d960a844d27c74db5c17f6ee9d6dae0e2d8
funera1 <funeral7776@gmail.com>
2025-01-26 19:50:54 +0900

    print hello

--- surtr/boot.zig
+++ surtr/boot.zig
@@ -2,8 +2,17 @@ const std = @import("std");
 const uefi = std.os.uefi;
 
 pub fn main() uefi.Status {
-    while (true)
-        asm volatile ("hlt");
+    var status: uefi.Status = undefined
+
+    const con_out = uefi.system_table.con_out orelse return .Aborted;
+    status = con_out.clearScreen();
+
+    for ("Hello, world!\n") |b| {
+        con_out.outputString(&[_:0]u16{ b }).err() catch unreachable;
+    }
+
+    // while (true)
+    //     asm volatile ("hlt");
     
     return .success;
 }
