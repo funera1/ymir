70239e1d518d8b9f622ff06e5f7be5d55e7a2e34
funera1 <funeral7776@gmail.com>
2025-01-26 19:36:10 +0900

    First commit

---
+++ build.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+
+    // Surtr Executable
+    const surtr = b.addExecutable(.{
+        .name = "BOOTX64.EFI",
+        .root_source_file = b.path("surtr/boot.zig"),
+        .target = b.resolveTargetQuery(.{
+            .cpu_arch = .x86_64,
+            .os_tag = .uefi,
+        }),
+        .optimize = optimize,
+        .linkage = .static,
+    });
+    b.installArtifact(surtr);
+}
