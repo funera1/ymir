66d72ddc0b46697d92fa7c6c80a945650230b8b0
funera1 <funeral7776@gmail.com>
2025-01-29 11:35:28 +0900

    UEFIのメモリマップを取得するためのクラスを作成

---
+++ surtr/defs.zig
@@ -0,0 +1,47 @@
+const std = @import("std");
+const uefi = std.os.uefi;
+
+// mapの定義
+pub const MemoryMap = extern struct {
+    /// Total buffer size prepared to store the memory map.
+    buffer_size: usize,
+    /// Memory descriptors.
+    descriptors: [*]uefi.tables.MemoryDescriptor,
+    /// Total memory map size.
+    map_size: usize,
+    /// Map key used to check if the memory map has been changed.
+    map_key: usize,
+    /// Size in bytes of each memory descriptor.
+    descriptor_size: usize,
+    /// UEFI memory descriptor version.
+    descriptor_version: u32,
+};
+
+// mapのイテレータ
+pub const MemoryDescriptorIterator = struct {
+    const Self = @This();
+    const Md = uefi.tables.MemoryDescriptor;
+
+    descriptors: [*]Md,
+    current: *Md,
+    descriptor_size: usize,
+    total_size: usize,
+
+    pub fn new(map: MemoryMap) Self {
+        return Self{
+            .descriptors = map.descriptors,
+            .current = @ptrCast(map.descriptors),
+            .descriptor_size = map.descriptor_size,
+            .total_size = map.map_size,
+        };
+    }
+
+    pub fn next(self: *Self) ?*Md {
+        if (@intFromPtr(self.current) >= @intFromPtr(self.descriptors) + self.total_size) {
+            return null;
+        }
+        const md = self.current;
+        self.current = @ptrFromInt(@intFromPtr(self.current) + self.descriptor_size);
+        return md;
+    }
+};
